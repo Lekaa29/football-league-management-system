bc09f13f8200b9c2853a7f86a5092a95cfc8c092
Lekaa29 <orvol905@gmail.com>
2023-11-21 02:01:12 +0100

    aaaa

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,22 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
+@auth.route("/login", methods=["GET", "POST"])
+def login():
+    data = request.form
+    print(data)
+    return render_template("login.html")
+
+@auth.route("/logout")
+def logout():
+    return "<p>logout</p>"
+
+@auth.route("/sign-up", methods=["GET", "POST"])
+def sign_up():
+    if request.method == "POST":
+        email = request.form.get("email")
+        username = request.form.get("username")
+        password = request.form.get("password1")
+        password = request.form.get("password2")
+    return render_template("signup.html")
